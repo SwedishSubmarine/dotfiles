5105f6a8823398fcf7e295d11fa97e99c99e4406
Emily Tiberg <emily.jo.tiberg@gmail.com>
2025-08-27 21:03:43 +0200

    🐈

--- home-manager/desktop-core/rofi/web-search.sh
+++ home-manager/desktop-core/rofi/web-search.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
 
 declare -A URLS
-
-# URL list changed from https://github.com/miroslavvidovic/rofi-scripts/blob/master/web-search.sh
-
 URLS=(
   ["duckduckgo"]="https://www.duckduckgo.com/?q="
   ["nixpkgs"]="https://search.nixos.org/packages?query="
   ["nixopts"]="https://search.nixos.org/options?query="
   ["home-manager"]="https://home-manager-options.extranix.com/?query="
 )
-
-# List for rofi
-gen_list() {
+list() {
     for i in "${!URLS[@]}"
     do
       echo "$i"
     done
 }
-
 main() {
-  # Pass the list to rofi
-  platform=$( (gen_list) | rofi -dmenu -matching fuzzy -no-custom -p "Platform > " )
+  platform=$( (list) | rofi -dmenu -matching fuzzy -no-custom -p "Platform > " )
 
   if [[ -n "$platform" ]]; then
     query=$( (echo ) | rofi  -dmenu -matching fuzzy -p "$platform > " )
@@ -37,7 +30,5 @@ main() {
     exit
   fi
 }
-
 main
-
 exit 0
