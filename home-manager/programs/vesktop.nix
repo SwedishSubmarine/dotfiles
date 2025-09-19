8dff3f57866eb0f8f57b59fafa4183596463bdb6
Emily Tiberg <emily.jo.tiberg@gmail.com>
2025-09-19 17:35:23 +0200

    explicit plugins vesktop

--- home-manager/programs/vesktop.nix
+++ home-manager/programs/vesktop.nix
@@ -7,9 +7,59 @@
         cur = theme.current.discordcss;
       };
       settings = {
+        autoUpdate = true;
+        autoUpdateNotification = true;
         enabledThemes = [
           "cur.css"
         ];
+        plugins = {
+          BetterFolders = {
+            enabled = true;
+            sidebar = true;
+            sidebarAnim = true;
+          };
+          BetterGifAltText = {
+            enabled = true;
+          };
+          CallTimer = {
+            enabled = true; 
+          };
+          CrashHandler = {
+            enabled = true;
+          };
+          CustomRPC = {
+            enabled = true;
+          };
+          ImageFilename = {
+            enabled = true;
+          };
+          MemberCount = {
+            enabled = true;
+          };
+          NoF1 = {
+            enabled = true;
+          };
+          NoTrack = {
+            enabled = true;
+            disableAnalytics = true;
+          };
+          petpet = {
+            enabled = true;
+          };
+          settings = {
+            enabled = true;
+            settingsLocation = "aboveNitro";
+          };
+          WebKeybinds = {
+            enabled = true;
+          };
+          WebScreenShareFixes = {
+            enabled = true;
+          };
+          YoutubeAdblock = {
+            enabled = true;
+          };
+        };
       };
     };
   };
