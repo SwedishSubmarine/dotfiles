6c4547815920e32888ad834e7e34ee37a08ec8e8
Emily Tiberg <emily.jo.tiberg@gmail.com>
2026-01-26 16:13:17 +0100

    why did they even do this

--- home-manager/terminal-core/git.nix
+++ home-manager/terminal-core/git.nix
@@ -2,19 +2,19 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "emily.jo.tiberg@gmail.com";
-    userName = "Emily Tiberg";
-    aliases = {
-      s = "status";
-      lg = ''log --oneline --graph --decorate --pretty=format:"%C(cyan)%h\ %ad%Cred%d \%Creset%s%Cblue" --date=short'';
-    };
     ignores = ["**/.DS_Store" ];
-    extraConfig = {
+    settings = {
       init.defaultBranch = "main";
       pull.rebase = true;
       core.editor = "nvim";
       core.symlinks = true;
       merge.autostash = true;
+      user.Email = "emily.jo.tiberg@gmail.com";
+      user.Name = "Emily Tiberg";
+      aliases = {
+        s = "status";
+        lg = ''log --oneline --graph --decorate --pretty=format:"%C(cyan)%h\ %ad%Cred%d \%Creset%s%Cblue" --date=short'';
+    };
     };
   };
 }
