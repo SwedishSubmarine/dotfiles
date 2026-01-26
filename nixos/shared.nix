ca523da57c7f0ff5d0497e184b76ddede69e7263
Emily Tiberg <emily.jo.tiberg@gmail.com>
2026-01-26 16:13:38 +0100

    eduroam doesnt like iwd (or rather im a bit lazy)

--- nixos/shared.nix
+++ nixos/shared.nix
@@ -3,13 +3,12 @@
   networking = {
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
     };
 
-    wireless.iwd = {
-      enable = true;
-      settings.General.EnableNetworkConfiguration = true;
-    };
+    # wireless.iwd = {
+    #   enable = true;
+    #   settings.General.EnableNetworkConfiguration = true;
+    # };
   };
 
   time.timeZone = "Europe/Stockholm";
